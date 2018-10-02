6f9dc04c83b4f89390dc94f5325b4119faadfba4
anlonglong <940752944@qq.com>
2018-10-02 16:50:24 +0800

    栈结结构实现了入栈，出栈，测试

--- src/com/allon/datastruct/note/Main.java
+++ src/com/allon/datastruct/note/Main.java
@@ -1,10 +1,46 @@
 package com.allon.datastruct.note;
 
-import com.allon.datastruct.note.sequence.SqList;
+import com.allon.datastruct.note.list.linkedlist.LinkedList;
+import com.allon.datastruct.note.list.sequencelist.SqList;
+import com.allon.datastruct.note.stack.Stack;
 
 public class Main {
 
     public static void main(String[] args) {
+//        sqList();
+//        linkList();
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        System.out.println("size = " + stack.size());
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        System.out.println("size = " + stack.size());
+
+    }
+
+    private static void linkList() {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        linkedList.listInsert(1,1);
+        linkedList.listInsert(2,2);
+        linkedList.listInsert(3,3);
+        linkedList.listInsert(4,7);
+        linkedList.listInsert(5,5);
+        linkedList.listInsert(6,0);
+        System.out.println("size = "+linkedList.length());
+        System.out.println("element = "+linkedList.getElement(4));
+        Integer integer = linkedList.listDelete(6);
+        System.out.println("integer = " + integer);
+        linkedList.cleanList();
+        System.out.println("size = "+linkedList.length());
+    }
+
+    private static void sqList() {
         SqList<Integer> sqList = new SqList<>();
         sqList.listInsert(1, 1);
         sqList.listInsert(2, 2);
---
+++ src/com/allon/datastruct/note/stack/Stack.java
@@ -0,0 +1,38 @@
+package com.allon.datastruct.note.stack;
+
+@SuppressWarnings("all")
+public class Stack<T> {
+
+private Object[] dataSource = new Object[10];
+private int top = -1;
+private int size;
+
+    public Stack() {
+    }
+
+    public void push(T data) {
+        //栈的存储元素的下表从0开始，所以减一
+        if (top == dataSource.length - 1) {
+            throw new IllegalStateException("栈已经满了");
+        }
+
+        top++;
+        dataSource[top] = data;
+        size++;
+    }
+
+    public T pop() {
+        if (top == -1) {
+            throw new IllegalStateException("栈为空");
+        }
+        T data = (T) dataSource[top];
+        top--;
+        size--;
+        return data;
+
+    }
+    public int size() {
+        return this.size;
+    }
+}
+
